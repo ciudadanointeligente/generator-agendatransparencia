1e0ae977fc3bbd68263c9a23543a348f14affbc4
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2015-12-10 15:46:16 -0300

    No testeando que hace un commit por que se muere todo

--- test/app.js
+++ test/app.js
@@ -43,12 +43,4 @@ describe('generator-agendatransparencia:app', function () {
       'a_transparencia/_cards/card-5.md'
     ]);
   });
-  it('hace un commit', function (done) {
-    var open = require('nodegit').Repository.open;
-    open('a_transparencia').then(function (repo) {
-      repo.getBranchCommit('gh-pages', function () {
-        done();
-      });
-    });
-  });
 });
